eeb2bab60393882507f026469eb13ac8a18f1d5f
Michael Gutman <michaelgutman@hotmail.com>
2017-01-22 16:42:21 -0500

    Rather than re-sending, wait for response

--- src/com/codenvy/example/swing/PeerConnection.java
+++ src/com/codenvy/example/swing/PeerConnection.java
@@ -2,6 +2,7 @@ package com.codenvy.example.swing;
 
 import java.io.*;
 import java.net.*;
+import java.util.Arrays;
 
 public class PeerConnection {
     String IP, id, peerID;
@@ -28,7 +29,6 @@ public class PeerConnection {
     
     
     public void handshake() throws IOException{      
-        //BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
     	System.out.println("attempting to open socket");
         Socket clientSocket = new Socket(IP, port);
         
@@ -36,57 +36,45 @@ public class PeerConnection {
         
         DataOutputStream os = new DataOutputStream(clientSocket.getOutputStream());
         DataInputStream is = new DataInputStream(clientSocket.getInputStream());
-        
+       
         os.writeByte(PSTRLEN);
         os.write(PSTR.getBytes());
         os.write(new byte[8]);
         os.write(infoHash);
         os.write(peerID.getBytes());
-        os.flush();
-        //System.out.println(is.read());
-        //serverHandshakeMessage = inFromServer.readLine();
-        //System.out.println("FROM SERVER: " + serverHandshakeMessage);
+
+    	while (is.available() == 0) {
+    		//wait for a response
+    	}
+    	
         try {
-        	while (is.available() == 0) {
-        		os.writeByte(PSTRLEN);
-                os.write(PSTR.getBytes());
-                os.write(new byte[8]);
-                os.write(infoHash);
-                os.write(peerID.getBytes());
-                os.flush();
-        	}
         	while (is.available() > 0) {
         		int inPSTRLEN = is.readByte();
-                byte[] inPSTR = new byte[inPSTRLEN];
-                is.readFully(inPSTR);
-                byte[] reserved = new byte[8];
-                is.readFully(reserved);
-                byte[] infoHashin = new byte[20];
-                is.readFully(infoHashin);
-                byte[] peerIdin = new byte[20];
-                is.readFully(peerIdin);
-                System.out.println("Data Recieved"); //figure out how and what to do now
+        		if (inPSTRLEN > 0) {
+	                byte[] inPSTR = new byte[inPSTRLEN];
+	                is.readFully(inPSTR);
+	                byte[] reserved = new byte[8];
+	                is.readFully(reserved);
+	                byte[] infoHashin = new byte[20];
+	                is.readFully(infoHashin);
+	                byte[] peerIdin = new byte[20];
+	                is.readFully(peerIdin);
+	                if (Arrays.equals(infoHashin, infoHash)) {
+	                	
+	                } else {
+	                	is.close();
+	                	os.close();
+	                	clientSocket.close();
+	                }
+        		}
         	}
         } 
         catch (EOFException e) {
-        	System.out.println("end of file");
+        	e.printStackTrace();
         	is.close();
         	os.close();
         	clientSocket.close();
         }
-//        byte temp[] = new byte[68];
-//        try {
-//        	while (true) {
-//        		System.out.println("reading...");
-//        		is.readFully(temp);
-//        		System.out.println(temp.toString());
-//        	}
-//        } catch(EOFException e) {
-//        	System.out.println("end of file");
-//        	is.close();
-//        	os.close();
-//        	clientSocket.close();
-//        }
         is.close();
         os.close();
         clientSocket.close();
